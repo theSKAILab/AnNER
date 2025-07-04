3956146b342935d40144cd69063657a775e10f31
Nicholas Pease <me@nicholaspease.com>
2025-07-04 15:22:00 -0400

    feat: reorganize start page to make less busy

--- src/components/pages/StartPage.vue
+++ src/components/pages/StartPage.vue
@@ -18,32 +18,18 @@
           Hint: You can also drag and drop files into this window!
         </p>
       </div>
-    </div>
-
-    <div class="row" style="position: absolute; bottom: 15px; width: 100%;">
-      <div class="col items-start start-footer justify-end">
-        This tool was designed and developed in the Spatial Knowledge and Artificial Intelligence (SKAI) lab at University of Maine<br/>
-        <div style="height: 45px; padding-top: 5px;" class="row items-center">
-          <div class="col col-md-auto" style="margin-right: 15px;">
-            <img src="@/assets/umaine-dark.png" alt="UMaine Logo" class="q-mb-sm" height="40px" v-show="$q.dark.isActive"/>
-            <img src="@/assets/umaine.png" alt="UMaine Logo" class="q-mb-sm" height="40px" v-show="!$q.dark.isActive"/>
-          </div>
-          <div class="col col-md-auto">
-            <img src="@/assets/skailabicon.png" style="height: 40px;"/>
-          </div>
-        </div>
+      <div class="start-footer">
+        This tool was designed and developed in the Spatial Knowledge and Artificial Intelligence (SKAI) lab at University of Maine. <br/>
+        The research was supported in part by the U.S. Department of Agriculture through a National Institute of Food and Agriculture (NIFA) grant<br/>
+        Award #<a href="https://portal.nifa.usda.gov/web/crisprojectpages/1025810-cellulose-materials-informatics-building-a-knowledge-graph-for-cellulose-materials-discovery-and-research-cellograph.html" target="_blank">2021-67022-34366</a>
+      <div class="image-logos">
+        <img src="@/assets/skailabicon.png"/>
+        <img src="@/assets/umaine-dark.png" alt="UMaine Logo" v-show="$q.dark.isActive"/>
+        <img src="@/assets/umaine.png" alt="UMaine Logo" v-show="!$q.dark.isActive"/>
+        <img src="@/assets/forestserviceicon.png"/>
       </div>
-      <div class="col start-footer justify-end" style="text-align: center;">
-        <span style="font-weight: bold;">Cite this tool as:</span><br />
-        Author 1, Author 2, Author 3, & Author 4. ({{ new Date().getFullYear() }}). 
-        <span style="font-style: italic;">Annotation and Named Entity Review Tool (AnNER)</span> [Computer software].
-      </div>
-      <div class="col items-end start-footer justify-end" style="text-align: right;">
-        This research was supported in part by the U.S. Department of Agriculture through a National Institute of Food and Agriculture (NIFA) grant, Award #<a href="https://portal.nifa.usda.gov/web/crisprojectpages/1025810-cellulose-materials-informatics-building-a-knowledge-graph-for-cellulose-materials-discovery-and-research-cellograph.html" target="_blank">2021-67022-34366</a><br/>
-        <img src="@/assets/forestserviceicon.png" style="height: 50px;margin-top: 15px;"/>
       </div>
     </div>
-
   </div>
 </template>
 
